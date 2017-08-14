e18d3d750affd7fbce198478631b975bf28ae50c
Carson Fleming <carsonf@syapse.com>
2017-08-14 15:33:12 -0700

    Attempting to fix initial installation

--- include/search_idx.php
+++ include/search_idx.php
@@ -14,6 +14,9 @@
 if (!defined('PUN'))
 	exit;
 
+// Make SITE_ID default to 0 for initial setup
+if (!defined('SITE_ID'))
+    define('SITE_ID', 0);
 
 // Make a regex that will match CJK or Hangul characters
 define('PUN_CJK_HANGUL_REGEX', '['.
