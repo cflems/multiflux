75c0e9a1cae232f6a860a707b03f534b7fe9ab56
Carson Fleming <carsonf@syapse.com>
2017-08-16 10:59:34 -0700

    Created new admin-panel branch

---
+++ create_site.php
@@ -0,0 +1,9 @@
+<?php
+define('PUN_ROOT', dirname(__FILE__).'/');
+require PUN_ROOT.'include/common.php';
+
+$page_title = array('Create Site', 'Multiflux Administration');
+require PUN_ROOT.'header.php';
+
+require PUN_ROOT.'footer.php';
+?>
