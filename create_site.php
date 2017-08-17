1d24dd26fac0f74bc1caa73bd363d81be2542976
Carson Fleming <carsonf@syapse.com>
2017-08-17 10:25:04 -0700

    Messing around with a create site page

--- create_site.php
+++ create_site.php
@@ -4,6 +4,13 @@ require PUN_ROOT.'include/common.php';
 
 $page_title = array('Create Site', 'Multiflux Administration');
 require PUN_ROOT.'header.php';
-
+?>
+    <div class="box">
+      <form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">
+        <p><input type="text" name="subdom" size="20" maxlength="30" /><span class="formlabel">.<?php echo $subdom_super; ?></span></p>
+        <p><input type="submit" name="create" value="Create Site" /></p>
+      </form>
+    </div>
+<?php
 require PUN_ROOT.'footer.php';
 ?>
