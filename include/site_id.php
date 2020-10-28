6eb9187a061ee68c8302eaf00118e26353408e31
Carson Fleming <cflems@cflems.net>
2020-10-28 04:28:26 +0100

    Potentially redirect if failing to get site ID

--- include/site_id.php
+++ include/site_id.php
@@ -7,9 +7,18 @@ function set_siteid_from_hostname ()
 {
   global $db;
 
-  $result = $db->query('SELECT site_id FROM '.$db->prefix.'hostmap WHERE host = \''.$db->escape(strtolower($_SERVER['HTTP_HOST'])).'\'') or error('Unable to fetch site ID', __FILE__, __LINE__, $db->error());
+  $result = $db->query('SELECT site_id FROM '.$db->prefix.'hostmap WHERE host = \''.$db->escape(strtolower($_SERVER['HTTP_HOST'])).'\'') or cant_get_siteid('Unable to fetch site ID', __FILE__, __LINE__, $db->error());
 
   if (!$db->num_rows($result)) error('No valid forum was found at this address'); // TODO: lang-ify
   else define('SITE_ID', $db->result($result));
+}
+
+// If the site ID can't be fetched, then initial database setup may be needed
+function cant_get_siteid ($message, $file = null, $line = null, $db_error = false)
+{
+	if (basename($_SERVER['PHP_SELF']) != 'install.php')
+		header('Location: install.php');
+
+	error($message, $file, $line, $db_error);
 }
 ?>
